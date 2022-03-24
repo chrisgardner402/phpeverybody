35157296dee46a42ca9ab74da563f20c4606bd52
chrisgardner402 <chojh3308@gmail.com>
2022-03-24 23:59:59 +0900

    section 18. session

---
+++ session/cookie1.php
@@ -0,0 +1,4 @@
+<?php
+setcookie('cookie1', '생활코딩');
+setcookie('cookie2', time(), time()+60);
+?>
---
+++ session/logout.php
@@ -0,0 +1,6 @@
+<?php
+ini_set("display_errors", "1");
+session_save_path('./session');
+session_destroy();
+header('Location: ./login.html');
+?>
