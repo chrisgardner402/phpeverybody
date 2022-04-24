93397f64d8cc7cbdcb7d9f58e259ab7af124c1e5
chrisgardner402 <chojh3308@gmail.com>
2022-04-24 21:13:51 +0900

    function and object 3

--- oop/compare.php
+++ oop/compare.php
@@ -12,4 +12,10 @@ var_dump($file->isFile());
 var_dump($file->isDir());
 var_dump($file->fread($file->getSize()));
 $file->fwrite(rand(1,100));
+
+$file2 = new SplFileObject('data2.txt', 'r+');
+var_dump($file2->isFile());
+var_dump($file2->isDir());
+var_dump($file2->fread($file2->getSize()));
+$file2->fwrite(rand(1,100));
 ?>
