fdad7af6599df8afe2339ea4a349d825564050c9
chrisgardner402 <chojh3308@gmail.com>
2022-04-26 21:03:56 +0900

    class loading and namespace 3 (namespace)

--- autoload/Hi.php
+++ autoload/Hi.php
@@ -1,7 +1,18 @@
 <?php
+namespace greeting\en;
 class Hi {
     function __construct() {
         echo '<h1>Hi</h1>';
     }
 }
+namespace greeting\ko;
+class Hi {
+    function __construct() {
+        echo '<h1>안녕</h1>';
+    }
+}
+use \greeting\en\Hi as HiEn;
+use \greeting\ko\Hi as HiKo;
+new HiEn();
+new HiKo();
 ?>
