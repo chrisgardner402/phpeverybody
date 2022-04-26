ad0fe76a7114db1366d8dc17063b4363bacd13b2
chrisgardner402 <chojh3308@gmail.com>
2022-04-26 22:04:19 +0900

    composer (dependencies)

---
+++ composerApp/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler(__DIR__ . '/app.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('EGO');
+$log->error('ING');
