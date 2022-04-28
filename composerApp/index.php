fd0af3cc062dc4936380777cb72d0bc5f0b69689
chrisgardner402 <chojh3308@gmail.com>
2022-04-28 22:33:20 +0900

    interface (monolog)

--- composerApp/index.php
+++ composerApp/index.php
@@ -3,11 +3,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\NativeMailerHandler;
 
 // create a log channel
 $log = new Logger('name');
-$log->pushHandler(new StreamHandler(__DIR__ . '/app.log', Logger::WARNING));
+$log->pushHandler(new StreamHandler(__DIR__ . '/app.log', Logger::ERROR));
+$log->pushHandler(new NativeMailerHandler('chojh3308@gmail.com', 'Emergency!!!!', 'out@system.com', Logger::EMERGENCY));
 
 // add records to the log
 $log->warning('EGO');
 $log->error('ING');
+$log->emergency('emergency');
